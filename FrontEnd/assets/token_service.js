22d4e0a02191a5ac446fb2114ba61e0cf9daaae3
Abou Ly <166803585+abou94190@users.noreply.github.com>
2026-06-04 11:37:06 +0200

    remove userID verif

--- FrontEnd/assets/token_service.js
+++ FrontEnd/assets/token_service.js
@@ -1,5 +1,4 @@
 // Fonction pour sauvegarder le token et l'ID utilisateur dans le localStorage
 export function token_sauvegarde(data) {
     localStorage.setItem('token', data.token);
-    localStorage.setItem('userId', data.userId);
 }
